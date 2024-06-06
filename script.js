551239d9057d49db4a6d6fa3020b01cdde30f630
swatkins14 <samuel.w92@hotmail.co.uk>
2024-06-06 18:29:42 +0100

    change

--- script.js
+++ script.js
@@ -4,6 +4,6 @@
 // console.log(`This is todays date ${date}`)
 
 let sentence = "I am a new sentence"
-function runsentence = sentence
+function runsentence { console.log(sentence)}
 
 console.log(runsentence);
